7cf165a05ac80f70f49f3d20a660ee185572fdc6
PH-266 <ph-266@PH-266.local>
2017-05-01 19:29:12 +0530

    add fb

--- android/app/src/main/java/com/firceail/MainApplication.java
+++ android/app/src/main/java/com/firceail/MainApplication.java
@@ -2,6 +2,9 @@ package com.firceail;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.wix.RNCameraKit.RNCameraKitPackage;
@@ -14,6 +17,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  protected static CallbackManager getCallbackManager(){
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
+              new FBSDKPackage(mCallbackManager),
             new RNCameraKitPackage()
       );
     }
@@ -39,6 +46,8 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    AppEventsLogger.activateApp(this);
+
   }
 }
